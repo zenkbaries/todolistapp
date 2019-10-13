412e1bff6dbe3f0f2b1a348ceb4db0a9a5f558f0
John Towery <kbaries@gmail.com>
2019-10-13 14:15:30 -0600

    format date to human readable

--- src/components/itemRow.js
+++ src/components/itemRow.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 export default class ItemRow extends Component {
 
+
     render() {
-        
+
         return (
             <tr>
                 <td>{this.props.list.list_status}</td>
                 <td>{this.props.list.list_item}</td>
-                <td>{this.props.list.list_due}</td>
-                <td>{this.props.list.list_created}</td>
+                <td>
+                    {new Date(this.props.list.list_due).toDateString()}
+                </td>
+                <td>
+                    <Link to={"/edit/"+this.props.list._id}>Edit</Link>
+                </td>
             </tr>
         );
     }

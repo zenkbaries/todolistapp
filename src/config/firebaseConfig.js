3fb13fa19cf70e18bb4fb2cc72e1411f4bb7f124
John Towery <kbaries@gmail.com>
2019-10-23 16:27:54 -0600

    add firebase config

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,22 @@
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/7.2.0/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+
+// <script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDzbe8zdGde8NujB3zKZ4ZKlxyQ0r9WeWU",
+    authDomain: "kb-todolistapp.firebaseapp.com",
+    databaseURL: "https://kb-todolistapp.firebaseio.com",
+    projectId: "kb-todolistapp",
+    storageBucket: "kb-todolistapp.appspot.com",
+    messagingSenderId: "928588325128",
+    appId: "1:928588325128:web:4bb32c6e197bfc9f98ccbd"
+  };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+// </script>
+
+export default firebaseConfig;

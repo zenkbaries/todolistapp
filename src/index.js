fada836f53485752a16fa8baed5c3644b00ffbb2
John Towery <kbaries@gmail.com>
2019-10-25 20:49:29 -0600

    comment out bootstrap to troubleshoot

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 // import $ from 'jquery';
 // import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Auth0Provider } from "./react-auth0-spa";
+import config from "./auth_config.json";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const onRedirectCallback = appState => {
+    window.history.replaceState(
+        {},
+        document.title,
+        appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+};
+
+ReactDOM.render(
+    <Auth0Provider
+        domain={config.domain}
+        client_id={config.clientId}
+        redirect_uri={window.location.origin}
+        onRedirectCallback={onRedirectCallback}
+    >
+        <App />
+    </Auth0Provider>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

3e3d839ab065a2fcaacb3fb67b697668135cb2c4
John Towery <kbaries@gmail.com>
2019-10-29 08:51:11 -0600

    refactor-remove td

--- src/components/itemRow.js
+++ src/components/itemRow.js
@@ -10,8 +10,6 @@ export default class ItemRow extends Component {
                 <td>
                     {new Date(this.props.list.list_due).toDateString()}
                 </td>
-                {/* <td><a href={"/edit/"+this.props.list._id} className="btn btn-info" role="button">Edit</a></td> */}
-                {/* <td><a href={"/delete/"+this.props.list._id} className="btn btn-info" role="button">Delete</a></td> */}
                 <td>
                     <a href={"/edit/"+this.props.list._id} className="btn btn-info" role="button">Edit</a>
                     <a href={"/delete/"+this.props.list._id} className="btn btn-danger" role="button">Delete</a>

c028ce7fe919e2ccff4bfb0409a1aede25be5e36
John Towery <kbaries@gmail.com>
2019-10-29 08:30:14 -0600

    removed 'status' from list for now

--- src/components/itemRow.js
+++ src/components/itemRow.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 export default class ItemRow extends Component {
 
 
@@ -7,7 +6,6 @@ export default class ItemRow extends Component {
 
         return (
             <tr>
-                <td>{this.props.list.list_status}</td>
                 <td>{this.props.list.list_item}</td>
                 <td>
                     {new Date(this.props.list.list_due).toDateString()}

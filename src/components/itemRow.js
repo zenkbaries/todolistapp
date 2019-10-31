cf887bca2b9f3586ffdf3d4be05ce14a866a22b9
John Towery <kbaries@gmail.com>
2019-10-31 12:39:42 -0600

    remove unneded code and add button

--- src/components/itemRow.js
+++ src/components/itemRow.js
@@ -1,34 +1,7 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import CompleteButton from 'CompleteButton';
 
 export default class ItemRow extends Component {
-    
-    constructor (props) {
-        super(props);
-        this.state = {
-            list_complete: false,
-        };
-    }
-    
-    async handleToggleComplete() {
-        axios.post(
-            'http://localhost:4000/lists/complete/'+this.props.list.id,
-            { 
-                list_complete: !this.props.list.list_complete,
-            }
-        )
-        .then(res => console.log(res.data))
-        .then(console.log("Now redirecting..."))
-        this.props.history.push("/");
-
-    }
-        // const response = await axios.post(
-        //     '/complete/THE_ID', 
-        //     { 
-        //         list_complete: !this.props.list.list_complete,
-        //     }
-        // );
-        // console.log(response);
 
     render() {
 
@@ -41,7 +14,7 @@ export default class ItemRow extends Component {
                 <td>
                     <a href={"/edit/"+this.props.list._id} className="btn btn-info" role="button">Edit</a>
                     <a href={"/delete/"+this.props.list._id} className="btn btn-danger" role="button">Delete</a>
-                    <button  onClick={() => this.toggleComplete()} className="btn btn-primary" >Complete</button>
+                    <CompleteButton item={this.props.list._id} />
 
                 </td>
 

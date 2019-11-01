acfbfe3968e07bce8b45b4cf8c57e0acaa460efe
John Towery <kbaries@gmail.com>
2019-11-01 09:40:03 -0600

    moved some code to itemRow from CompleteButton

--- src/components/CompleteButton.js
+++ src/components/CompleteButton.js
@@ -1,105 +1,17 @@
 import React, {Component} from 'react';
-import axios from 'axios';
-
 
 class CompleteButton extends Component {
-
-    constructor(props){
-        super(props);
-        this.state = {
-            complete: null,
-            isLoading: false,
-        };
-        this.click = this.click.bind(this);
-    }
-
-    componentDidMount() {
-        axios.get('http://localhost:4000/lists/'+this.props.item)
-            .then(
-                response => {
-                    this.setState(
-                        {
-                            complete: response.data.list_complete
-                        }
-                    )
-                    console.log('response:'+response.data.list_complete);
-                    console.log('complete:'+this.state.complete);  
-                },
-            )
-            .then (console.log('get:'+this.state.complete))
-            .catch(
-                function (error) {
-                    console.log(error);
-                }
-            )
-    }
-
-    // componentDidUpdate() {
-
-    //     const obj = {
-    //         list_complete: this.state.list_complete
-    //     }
-    //     axios.post('http://localhost:4000/lists/complete/'+this.props.item, obj)
-    //         .then(
-    //             (response) => {
-    //               this.setState({ isLoading: false });
-    //               console.log('response:',response);
-    //             }
-    //         )
-    //         .then(
-    //             console.log('axiosed')
-    //         )
-    //         .catch(
-    //             (err) => {
-    //               this.setState({ isLoading: false });
-    //               console.log(err);
-    //               console.log('id:'+this.props.item);
-    //             }
-    //          );
-    // }
-
-    click() {
-        console.log('clicked');
-        console.log('complete:'+this.state.complete);
-
-        this.setState(
-            { 
-                isLoading: true,
-                complete: !this.state.complete
-            }
-        );
-        console.log('setstate');
-        console.log('complete:'+this.state.complete);
-        // console.log('list_complete:'+response.data.list_complete);
-  
-        const obj = {
-            list_complete: this.state.complete
-        }
-        console.log(this.state.complete);
-
-        axios.post('http://localhost:4000/lists/complete/'+this.props.item, obj)
-            .then(
-                (response) => {
-                  this.setState({ isLoading: false });
-                  console.log('response:',response);
-                }
-            )
-            .then(
-                console.log('axiosed')
-            )
-            .catch(
-                (err) => {
-                  this.setState({ isLoading: false });
-                  console.log(err);
-                  console.log('id:'+this.props.item);
-                }
-             );
-    } // end Click()
 
     render() {
        return  (
             <div>
-                <button className="btn btn-primary" onClick={ this.click} disabled={this.state.isLoading}> Completed?</button>
+                <button 
+                    className="btn btn-primary" 
+                    onClick={this.props.onClick} 
+                    disabled={this.props.isLoading}
+                >
+                    Complete
+                </button>
             </div>
            );
     }
--- src/components/itemRow.js
+++ src/components/itemRow.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import CompleteButton from './CompleteButton';
+import axios from 'axios';
 
 export default class ItemRow extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            complete: this.props.list.list_complete,
+            isLoading: false,
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
     handleClick() {
-        console.log('clicked');
-        console.log('complete:'+this.state.complete);
 
         this.setState(
             { 
@@ -13,30 +21,21 @@ export default class ItemRow extends Component {
                 complete: !this.state.complete
             }
         );
-        console.log('setstate');
-        console.log('complete:'+this.state.complete);
-        // console.log('list_complete:'+response.data.list_complete);
   
         const obj = {
             list_complete: this.state.complete
         }
-        console.log(this.state.complete);
 
-        axios.post('http://localhost:4000/lists/complete/'+this.props.item, obj)
+        axios.post('http://localhost:4000/lists/complete/'+this.props.list._id, obj)
             .then(
                 (response) => {
                   this.setState({ isLoading: false });
-                  console.log('response:',response);
                 }
-            )
-            .then(
-                console.log('axiosed')
             )
             .catch(
                 (err) => {
                   this.setState({ isLoading: false });
                   console.log(err);
-                  console.log('id:'+this.props.item);
                 }
              );
     } // end Click()
@@ -52,9 +51,9 @@ export default class ItemRow extends Component {
                 <td>
                     <a href={"/edit/"+this.props.list._id} className="btn btn-info" role="button">Edit</a>
                     <a href={"/delete/"+this.props.list._id} className="btn btn-danger" role="button">Delete</a>
-                    <CompleteButton onClick={() => this.handleClick()} />
+                    <CompleteButton onClick={() => this.handleClick() } />
                 </td>
-                <td>{(this.props.list.list_complete) ? 'Yes' : 'No'}</td>
+                <td>{(this.state.complete) ? 'Yes' : 'No'}</td>
 
             </tr>
         );
